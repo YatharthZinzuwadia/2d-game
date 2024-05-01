3cbad06dc22763d7f16685a219dd00a7a350e63f
YatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2024-05-01 14:27:29 +0530

    first

---
+++ src/constants-explicit.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my old laptop. I work mostly in JavaScript as a web dev, but have slowly started learning towards react-native these days.
+    I've made a couple of Projects in that language. I also like MERN and React. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/YatharthZinzuwadia" target="_blank">Github</a>!`,
+  "cs-degree": `This is my IT degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here, play games and watch YouTube.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp</a>, <a href="https://www.youtube.com/@thesheryianscodingschool" target="_blank">Sheriyans</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@akshaymarch7" target="_blank">Akshay Saini</a>!`,
+  bed: `This where I sleep. Great ideas) (or lethargy) comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my portfolio. <a href="https://yatharthzinzuwadia.github.io/portfolio-react/" target="_blank">Check it out?</a>
+  Contact me at yatharthzinzuwadia@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of the tutorials I followed <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a lot of *ahem* "sTuDy mAtErIaL" programming books on my shelves.`,
+  exit: `If you want to exit my portfolio, just close the tab.`,
+};
