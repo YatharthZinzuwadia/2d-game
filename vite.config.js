bafaacfda379c81af846be4de049262e60a6ec03
YatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2024-05-01 14:36:10 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "./",
+  base: "https://github.com/YatharthZinzuwadia/2d-game",
   build: {
     minify: "terser",
   },
